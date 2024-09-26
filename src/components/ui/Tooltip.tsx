bb7e4db410bae4e1d22eb6bc421cf014cb7ff27e
smithfmt@gmail.com <smithfmt@gmail.com>
2024-09-26 13:36:08 +0100

    adjust contact section

--- src/components/ui/Tooltip.tsx
+++ src/components/ui/Tooltip.tsx
@@ -1,7 +1,7 @@
 const Tooltip = ({ text, closed, contact }: { text: string, closed: boolean, contact?:boolean }) => {
 
     return (
-        <div className={`fixed ${contact?"bottom-32":"bottom-16"} right-5 p-2 px-4 text-sm ${closed ? "opacity-0 pointer-events-none" : "opacity-100"} button-glow-white`}>
+        <div className={`fixed ${contact?"-bottom-12 lg:bottom-32 md:right-16":"bottom-16"} right-5 p-2 px-4 text-sm ${closed ? "opacity-0 pointer-events-none" : "opacity-100"} button-glow-white`}>
             {text}
         </div>
     );
